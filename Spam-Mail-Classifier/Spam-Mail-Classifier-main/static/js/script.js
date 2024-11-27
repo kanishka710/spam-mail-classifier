cf12ecdfac418fd3975f51bf9fc1211868717899
kanishka710 <kanishkachaturvedi07@gmail.com>
2024-11-27 15:24:38 +0530

    update

--- Spam-Mail-Classifier/Spam-Mail-Classifier-main/static/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const predictionAlert = document.querySelector('.alert');
-    if (predictionAlert) {
-        setTimeout(() => {
-            predictionAlert.style.display = 'none';
-        }, 3000); 
-    }
-});
