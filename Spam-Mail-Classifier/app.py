1942b5f8758fee95f6183238d0b79bd2226b0d35
acomightyarcher <acomightyarcher@gmail.com>
2024-11-27 16:32:51 +0530

    Changed the file paths

--- Spam-Mail-Classifier/app.py
+++ Spam-Mail-Classifier/app.py
@@ -15,8 +15,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
-model = pickle.load(open(r'Spam-Mail-Classifier\classifer.pkl', 'rb'))
-vectorizer = pickle.load(open(r'Spam-Mail-Classifier\vectorizer.pkl', 'rb'))  
+model = pickle.load(open(r'Spam-Mail-Classifier/classifer.pkl', 'rb'))
+vectorizer = pickle.load(open(r'Spam-Mail-Classifier/vectorizer.pkl', 'rb'))  
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
